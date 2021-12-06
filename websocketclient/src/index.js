363b2525366d5d4cd1b20dd8b78757ea273773b4
mohittk <mvk1407@gmail.com>
2021-12-06 21:52:48 +0530

    edited websocket backend

--- websocketclient/src/index.js
+++ websocketclient/src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom';
 import {w3cwebsocket as W3CWebSocket} from 'websocket';
 
 
-const client = new W3CWebSocket('ws://localhost:8000');
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
 export default class App extends Component{
 
     componentDidMount(){
         client.onopen = () =>{
             console.log('websocket client connected');
-        }
+        };
     }
     render(){
         return (
             <div>hey! this is fucking </div>
 
         )
-    }
+    }  
 }
 
 ReactDOM.render(
